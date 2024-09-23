a20659b58952c18ba1eb8ee8cdde54e173a920bd
Gustav Ivering <gustav.ivering@live.se>
2024-09-23 10:05:37 +0200

    Added converter for icons, aswell as navigation works as expected now

---
+++ SmartHub/Converters/DeviceTypeConverter.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+using System.Globalization;
+
+namespace SmartHub.Converters
+{
+    public class DeviceTypeConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is string deviceType)
+            {
+                return deviceType.ToLower() switch
+                {
+                    "light" => "fa-regular fa-lightbulb",
+                    "fan" => "fa-regular fa-fan",
+                    "temp" => "fa-sharp fa-light fa-temperature-high",
+                    _ => "fa-regular fa-microchip"
+                };
+            }
+            return "fa-regular fa-microchip";
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            try
+            {
+                throw new NotImplementedException();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                return null!;
+            }
+        }
+    }
+}
