dca82dc5ad2a7d35056acc49175bc9db3883e82f
Gustav Ivering <gustav.ivering@live.se>
2024-09-24 13:58:24 +0200

    Updated temperature device, so now all 3 devices are up and running

--- SmartTemperature/MainWindow.xaml.cs
+++ SmartTemperature/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using SharedResources.Handlers;
+using SmartTemperature.ViewModels;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,9 +18,28 @@ namespace SmartTemperature
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        public MainWindow(MainWindowVM viewModel)
         {
             InitializeComponent();
+            DataContext = viewModel;
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            AppDomain.CurrentDomain.ProcessExit += async (s, e) =>
+            {
+                var connectionString = "HostName=gurra-iothub.azure-devices.net;DeviceId=798197a0-e07f-453c-92c5-9a2121a2d673;SharedAccessKey=KaJ+2FmYJPW2L2OVn84wX5fSC3hgVHCbfAIoTFAUVuA=";
+                var dc = new DeviceClientHandler("798197a0-e07f-453c-92c5-9a2121a2d673", "SmartTemp", "Temp", connectionString);
+
+                await dc.DisconnectAsync(connectionString);
+            };
+
+            Application.Current.Shutdown();
+        }
+
+        private void TopWindowBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
         }
     }
 }
