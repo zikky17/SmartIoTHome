e6bfce1db460dd51e37f9097aebba00764c00ad3
Gustav Ivering <gustav.ivering@live.se>
2024-09-25 16:18:24 +0200

    Settings Page is now showing fan device info

--- SmartFan/ViewModels/SettingsVM.cs
+++ SmartFan/ViewModels/SettingsVM.cs
@@ -1,12 +1,44 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using Microsoft.Extensions.DependencyInjection;
+using SharedResources.Data;
+using SharedResources.Models;
 
 namespace SmartFan.ViewModels
 {
-    public partial class SettingsVM(IServiceProvider serviceProvider) : ObservableObject
+    public partial class SettingsVM : ObservableObject
     {
-        private readonly IServiceProvider _serviceProvider = serviceProvider;
+        private readonly IServiceProvider _serviceProvider;
+        private readonly IDatabaseContext _databaseContext;
+
+        private DeviceSettings _deviceSettings;
+        public DeviceSettings DeviceSettings
+        {
+            get => _deviceSettings;
+            set
+            {
+                _deviceSettings = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public SettingsVM(IDatabaseContext databaseContext, IServiceProvider serviceProvider)
+        {
+            _databaseContext = databaseContext;
+            _serviceProvider = serviceProvider;
+
+            LoadSettingsAsync().ConfigureAwait(false);
+
+        }
+
+        private async Task LoadSettingsAsync()
+        {
+            var result = await _databaseContext.GetSettingsAsync("cabb9896-0fba-47d2-b67d-0279a9745284");
+            if (result.Succeeded)
+            {
+                DeviceSettings = result.Content;
+            }
+        }
 
         [RelayCommand]
         private void GoToHome()
