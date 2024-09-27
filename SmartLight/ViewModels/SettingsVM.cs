0a17b0ac5d343bb7e4f5f492b8a70a7e314ade28
Gustav Ivering <gustav.ivering@live.se>
2024-09-27 11:31:52 +0200

    Settings page now updated for all 3 devices

--- SmartLight/ViewModels/SettingsVM.cs
+++ SmartLight/ViewModels/SettingsVM.cs
@@ -1,6 +1,8 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using Microsoft.Extensions.DependencyInjection;
+using SharedResources.Data;
+using SharedResources.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +11,52 @@ using System.Threading.Tasks;
 
 namespace SmartLight.ViewModels
 {
-    public partial class SettingsVM(IServiceProvider serviceProvider) : ObservableObject
+    public partial class SettingsVM : ObservableObject
     {
-        private readonly IServiceProvider _serviceProvider = serviceProvider;
+        private readonly IServiceProvider _serviceProvider;
+        private readonly IDatabaseContext _databaseContext;
+
+        private DeviceSettings _deviceSettings;
+        public DeviceSettings DeviceSettings
+        {
+            get => _deviceSettings;
+            set
+            {
+                _deviceSettings = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public SettingsVM(IDatabaseContext databaseContext, IServiceProvider serviceProvider)
+        {
+            _databaseContext = databaseContext;
+            _serviceProvider = serviceProvider;
+
+            LoadSettingsAsync().ConfigureAwait(false);
+
+        }
+
+        private async Task LoadSettingsAsync()
+        {
+            var result = await _databaseContext.GetSettingsAsync("f0468151-3b8b-4d92-8e42-cf679a27796f");
+            if (result.Succeeded)
+            {
+                DeviceSettings = result.Content;
+            }
+        }
+
+        [RelayCommand]
+        private async Task ResetSettings(DeviceSettings device)
+        {
+            var result = await _databaseContext.DeleteDeviceSettingsAsync(device);
+            if (result.Succeeded)
+            {
+                await _databaseContext.GetSettingsAsync(device.Id);
+                var mainWindow = _serviceProvider.GetRequiredService<MainWindowVM>();
+                mainWindow.CurrentViewModel = _serviceProvider.GetRequiredService<SettingsVM>();
+            }
+        }
+
 
         [RelayCommand]
         private void GoToHome()
--- SmartTemperature/ViewModels/SettingsVM.cs
+++ SmartTemperature/ViewModels/SettingsVM.cs
@@ -1,17 +1,56 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using SharedResources.Data;
+using SharedResources.Models;
 
 namespace SmartTemperature.ViewModels
 {
-    public partial class SettingsVM(IServiceProvider serviceProvider) : ObservableObject
+    public partial class SettingsVM : ObservableObject
     {
-        private readonly IServiceProvider _serviceProvider = serviceProvider;
+        private readonly IServiceProvider _serviceProvider;
+        private readonly IDatabaseContext _databaseContext;
+
+        private DeviceSettings _deviceSettings;
+        public DeviceSettings DeviceSettings
+        {
+            get => _deviceSettings;
+            set
+            {
+                _deviceSettings = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public SettingsVM(IDatabaseContext databaseContext, IServiceProvider serviceProvider)
+        {
+            _databaseContext = databaseContext;
+            _serviceProvider = serviceProvider;
+
+            LoadSettingsAsync().ConfigureAwait(false);
+
+        }
+
+        private async Task LoadSettingsAsync()
+        {
+            var result = await _databaseContext.GetSettingsAsync("798197a0-e07f-453c-92c5-9a2121a2d673");
+            if (result.Succeeded)
+            {
+                DeviceSettings = result.Content;
+            }
+        }
+
+        [RelayCommand]
+        private async Task ResetSettings(DeviceSettings device)
+        {
+            var result = await _databaseContext.DeleteDeviceSettingsAsync(device);
+            if (result.Succeeded)
+            {
+                await _databaseContext.GetSettingsAsync(device.Id);
+                var mainWindow = _serviceProvider.GetRequiredService<MainWindowVM>();
+                mainWindow.CurrentViewModel = _serviceProvider.GetRequiredService<SettingsVM>();
+            }
+        }
 
         [RelayCommand]
         private void GoToHome()
