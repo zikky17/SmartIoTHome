d40888ec2a9a3be3fae6ab5cf4005eb03f416f7a
Gustav Ivering <gustav.ivering@live.se>
2024-09-30 13:21:33 +0200

    Reworked home.razor layout to better fit more screens

--- SmartHub/Platforms/Windows/App.xaml.cs
+++ SmartHub/Platforms/Windows/App.xaml.cs
@@ -22,21 +22,6 @@ namespace SmartHub.WinUI
             this.InitializeComponent();
         }
 
-        protected override void OnLaunched(LaunchActivatedEventArgs args)
-        {
-            base.OnLaunched(args);
-
-            const int width = 400;
-            const int height = 700;
-
-            var window = Application.Windows[0].Handler.PlatformView as Microsoft.UI.Xaml.Window;
-            IntPtr windowHandle = WinRT.Interop.WindowNative.GetWindowHandle(window);
-            var windowId = Win32Interop.GetWindowIdFromWindow(windowHandle);
-            var appWindow = AppWindow.GetFromWindowId(windowId);
-
-            appWindow.Resize(new SizeInt32(width, height));
-        }
-
         protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
     }
 
